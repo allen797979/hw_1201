e92785f27c1a7be39e7c828fb58059639672f5c4
unknown <allenma930709@gmail.com>
2022-12-01 00:49:23 +0800

    hw_1201

---
+++ 13/source/Main.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void cubeByBeference(int &nPtr);
+
+int main(void)
+{
+	int number = 5;
+	printf("The original value of number is %d", number);
+
+	cubeByReference( number );
+
+	printf("\nThe new value of number is %d\n", number);
+	system("pause");
+	return 0;
+}
+
+void cubeByReference(int &nPtr)
+{
+	nPtr = nPtr * nPtr * nPtr;
+}
